aa85b42f236ce3053ef3f61ee2f8f1d0ff8efaac
Rodisolomon <hanzhit@uchciago.edu>
2023-09-04 23:08:34 -0700

    working

--- try_sele.py
+++ try_sele.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
 def test(): 
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())) #need to install selenium and webdriver-manager
     driver.get("https://www.selenium.dev/selenium/web/inputs.html")
     title = driver.title
-    assert title == "Web form"
+    print(title)
+    assert title == "inputs"
     # Click on the element 
     driver.find_element(By.NAME, "color_input")
     driver.quit()
