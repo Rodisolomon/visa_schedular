fcfd626e48b35a4555b23da28a6a88ce246c507e
Rodisolomon <hanzhit@uchciago.edu>
2023-09-07 14:10:39 -0700

    basic backend

--- src/app.py
+++ src/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import visit
+
 app = Flask(__name__)
 @app.route("/")
 def index():
     return "hello world"
 
+@app.route('/visit_nd')
+def call_nd():
+    visit.run_function_wrapper(60, visit.nextdoor)
+
 if __name__ == "__main__":
     app.run()
+
