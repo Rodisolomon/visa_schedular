a63b2da1a62cca6fe4494809c59adc56433956e4
Rodisolomon <hanzhit@uchciago.edu>
2023-10-16 02:08:29 -0500

    add global var

---
+++ src/global_variables.py
@@ -0,0 +1,4 @@
+CHECK_INTERVAL_IN_SEC = 60
+TIME_CHECK_EMAIL = "Hollyyhw@gmail.com"
+TIME_CHECK_PASSWORD = "Hollyyihaowu6"
+CANADA_CITY_LIST = ["Calgary", "Ottawa", "Toronto", "Vancouver", "Halifax", "Montreal", "Quebec City"]
