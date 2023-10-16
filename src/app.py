6e19a4fd2123bfad5145322f5aefa2bf5ce52a75
Helen Tan <hanzhitan@Helens-MacBook-Air.local>
2023-10-16 01:53:54 -0500

    comments and revise

--- src/app.py
+++ src/app.py
@@ -9,7 +9,8 @@ def index():
 
 @app.route('/visit_nd')
 def call_nd():
-    visit.run_function_wrapper(CHECK_INTERVAL_IN_SEC, visit.nextdoor)
+    visit.VisaAppointment()
+    visit.run_function_wrapper(CHECK_INTERVAL_IN_SEC, visit.update_and_check_if_reschedule)
 
 if __name__ == "__main__":
     app.run()
