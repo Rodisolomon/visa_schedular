c53a42c1c88da2a7706298018bb603e577e40a66
Helen Tan <thzhelen@gmail.com>
2023-11-27 18:51:17 -0600

    restructure

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 import web_api
 from global_variables import CHECK_INTERVAL_IN_SEC
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
-import src.web_api
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
+from app import index
 def test_index():
     assert index() == "hello world"
