9ce88fcc85091a8fe800207ff4d022db27ee5d0a
Helen Tan <thzhelen@gmail.com>
2023-11-27 19:02:04 -0600

    change test

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,6 @@ import web_api
 from global_variables import CHECK_INTERVAL_IN_SEC
 
 reschedular = web_api.VisaAppointment()
-reschedular.check_recent_available_date()
+# reschedular.check_recent_available_date()
 
 # reschedular.reschedule_for_users()
