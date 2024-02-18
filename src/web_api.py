5b183464c8f8dcdb23ac3701629bb94d85fbe3d6
Helen Tan <thzhelen@gmail.com>
2024-02-18 14:43:58 -0600

    update global var

--- src/web_api.py
+++ src/web_api.py
@@ -310,33 +310,3 @@ class VisaAppointment():
         self.reschedule_for_users()
         
         
-
-
-def nextdoor():
-    """
-    small test to see if selenium work as expected
-    """
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())) #need to install selenium and webdriver-manager
-    driver.get("https://www.nextdoor.com/")  
-
-    #login
-    account = driver.find_element(By.XPATH, '//input[@type="email"]')
-    account.send_keys("thzhelen@gmail.com")
-    password = driver.find_element(By.XPATH, '//input[@type="password"]')
-    password.send_keys("thz1123helenTG!")
-    continue_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
-    continue_button.submit()
-    time.sleep(3)
-
-    #publish post
-    driver.get("https://nextdoor.com/news_feed/#navigation")
-    post = driver.find_element(By.XPATH, '//button[contains(.,"Post")]')
-    post.click()
-    input_box = driver.find_element(By.XPATH, '//textarea[@class="postbox-field-textarea-with-tags-input postbox-field-textarea _icIOf5X"]')
-    input_box.send_keys("try to publish this post")
-    time.sleep(1) #there's a change in postion if button, have to sleep
-    post_publish = driver.find_element(By.XPATH, '//button[contains(@aria-label, "composer submit button")]')
-    post_publish.click()
-    time.sleep(3)
-
-    driver.quit()  
